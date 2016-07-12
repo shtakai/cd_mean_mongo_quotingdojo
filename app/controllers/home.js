ae2ac9460b0525d50e29f07c299f635ef4d89023
Alyson Takai <shtakai@gmail.com>
2016-07-11 18:49:37 -0700

    Quote work

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,9 +1,14 @@
+const mongoose = require('mongoose');
+const Quote = mongoose.model('Quote');
 
 /*!
  * Module dependencies.
  */
 
 exports.index = function (req, res) {
+  let quote = new Quote();
+  quote.name = 'test';
+  quote.save(function(err){});
   res.render('home/index', {
     title: 'Node Express Mongoose Boilerplate'
   });
